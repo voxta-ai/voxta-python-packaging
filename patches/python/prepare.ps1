d1c795e2829f61b42299f9a06fc0378b6030709f
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-12-27 10:05:47 -0500

    Configure actions for python, deepspeed and tts

---
+++ patches/python/prepare.ps1
@@ -0,0 +1,6 @@
+$scriptFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$pythonFolder = $args[0]
+Remove-Item $(Join-Path $pythonFolder "Tools/demo") -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $(Join-Path $pythonFolder "Doc") -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $(Join-Path $pythonFolder "Lib/test") -Recurse -Force -ErrorAction SilentlyContinue
+Copy-Item $(Join-Path $scriptFolder "voxta-python.bat") $pythonFolder/voxta-python.bat
---
+++ patches/python/voxta-python.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+echo Entering Voxta Python Virtual Environment
+set "PYTHONHOME=%~dp0"
+set "PYTHONPATH=%PYTHONHOME%Lib\site-packages\;%PYTHONHOME%Lib\;%PYTHONHOME%DLLs\;%PYTHONHOME%"
+set "PATH=%PYTHONHOME%Scripts;%PATH%"
+set "LIBPATH=%PYTHONHOME%libs"
+echo PYTHONHOME = %PYTHON_HOME%
+echo PYTHONPATH = %PYTHONPATH%
+echo LIBPATH = %LIBPATH%
+echo PATH = %PATH%
+echo Launching cmd with Voxta Python Virtual Environment. Type exit to exit.
+cmd
+echo Exited Voxta Python Virtual Environment
+endlocal
