8223233a4d02a0283e9aaff018bb356c4304d70e
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-12-29 20:52:01 -0500

    Configure DeepSpeed v0.11.1

--- patches/python/prepare.ps1
+++ patches/python/prepare.ps1
@@ -3,4 +3,5 @@ $pythonFolder = $args[0]
 Remove-Item $(Join-Path $pythonFolder "Tools/demo") -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item $(Join-Path $pythonFolder "Doc") -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item $(Join-Path $pythonFolder "Lib/test") -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $(Join-Path $pythonFolder "Scripts/pip*") -Recurse -Force -ErrorAction SilentlyContinue
 Copy-Item $(Join-Path $scriptFolder "voxta-python.bat") $pythonFolder/voxta-python.bat
